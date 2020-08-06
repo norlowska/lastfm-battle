9084bf5526e1c0faf415824f5cf769b263a7deab
Natalia <mcb.nat@gmail.com>
2020-08-06 19:05:59 +0200

    fix: Don't allow entering two identical usernames

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
-import "./style.css";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import './style.css';
 
 /*
  *  Home Page
@@ -12,7 +12,12 @@ import "./style.css";
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { username1: "", username2: "" };
+    this.state = {
+      username1: '',
+      username2: '',
+      showValidationMessage: false,
+      validationMessage: '',
+    };
   }
 
   onChange(event) {
@@ -22,45 +27,55 @@ class Home extends Component {
   // Go to battle page on form submit
   onSubmit(event) {
     event.preventDefault();
-    let path = "/battle/" + this.state.username1 + "/" + this.state.username2;
-    this.props.history.push(path);
+    if (this.state.username1.toLowerCase() === this.state.username2.toLowerCase()) {
+      this.setState({
+        showValidationMessage: true,
+        validationMessage: 'You have entered two identical usernames. Change one of them.',
+      });
+    } else {
+      let path = '/battle/' + this.state.username1 + '/' + this.state.username2;
+      this.props.history.push(path);
+    }
   }
 
   render() {
     return (
-      <div className="home-container">
-        <header className="large-logo">
+      <div className='home-container'>
+        <header className='large-logo'>
           <h1>
-            Last.fm <span className="logo-white">battle</span>
+            Last.fm <span className='logo-white'>battle</span>
           </h1>
         </header>
-        <form className="home-form" onSubmit={this.onSubmit.bind(this)}>
-          <div className="form-group left">
+        <form className='home-form' onSubmit={this.onSubmit.bind(this)}>
+          <div className='form-group left'>
             <input
-              className="username-input"
-              type="text"
-              name="username1"
-              id="input-user1"
+              className='username-input'
+              type='text'
+              name='username1'
+              id='input-user1'
               value={this.state.username1}
               onChange={this.onChange.bind(this)}
               required
             ></input>
-            <label htmlFor="input-user1">Username #1</label>
+            <label htmlFor='input-user1'>Username #1</label>
           </div>
-          <div className="form-group right">
+          <div className='form-group right'>
             <input
-              className="username-input"
-              type="text"
-              name="username2"
-              id="input-user2"
+              className='username-input'
+              type='text'
+              name='username2'
+              id='input-user2'
               value={this.state.username2}
               onChange={this.onChange.bind(this)}
               required
             ></input>
-            <label htmlFor="input-user2">Username #2</label>
+            <label htmlFor='input-user2'>Username #2</label>
           </div>
-          <div className="form-group submit">
-            <button className="battle-button" type="submit">
+          {this.state.showValidationMessage ? (
+            <div className='validation-message'>{this.state.validationMessage}</div>
+          ) : null}
+          <div className='form-group submit'>
+            <button className='battle-button' type='submit'>
               battle
             </button>
           </div>
@@ -71,7 +86,7 @@ class Home extends Component {
 }
 
 Home.propTypes = {
-  history: PropTypes.object
+  history: PropTypes.object,
 };
 
 export default withRouter(Home);
